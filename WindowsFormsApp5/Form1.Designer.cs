e4d56d70627652915c405b71faf3ffa8bef558dc
KaragozE <karagoz.eray@gmail.com>
2018-11-27 12:00:36 +0300

    first commit

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbAirplane = new System.Windows.Forms.TextBox();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.cmbSelect = new System.Windows.Forms.ComboBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CompanyName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phone";
+            // 
+            // tbAirplane
+            // 
+            this.tbAirplane.Location = new System.Drawing.Point(151, 24);
+            this.tbAirplane.Name = "tbAirplane";
+            this.tbAirplane.Size = new System.Drawing.Size(121, 20);
+            this.tbAirplane.TabIndex = 4;
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Location = new System.Drawing.Point(151, 53);
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(121, 20);
+            this.tbPhone.TabIndex = 5;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(437, 37);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(181, 23);
+            this.btnAddNew.TabIndex = 8;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // cmbSelect
+            // 
+            this.cmbSelect.FormattingEnabled = true;
+            this.cmbSelect.Location = new System.Drawing.Point(437, 67);
+            this.cmbSelect.Name = "cmbSelect";
+            this.cmbSelect.Size = new System.Drawing.Size(181, 21);
+            this.cmbSelect.TabIndex = 9;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(533, 104);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(85, 23);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(437, 338);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(181, 23);
+            this.btnRefresh.TabIndex = 12;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(603, 189);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 373);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.cmbSelect);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.tbPhone);
+            this.Controls.Add(this.tbAirplane);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbAirplane;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.ComboBox cmbSelect;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection conn = new SqlConnection();
+        private string conString = "Data Source=DMGM0349997\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+        private SqlCommand cmd;
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshData();
+        }
+        private void handleExeption(Exception ex)
+        {
+
+            string msg = ex.Message.ToString();
+            string caption = "error";
+
+            MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void refreshData()
+        {
+
+
+            conn.ConnectionString = conString;
+            cmd= conn.CreateCommand();
+
+            try
+            {
+                string query = "select * from Shippers";
+                cmd.CommandText = query;
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dataGridView1.DataSource = dt;
+                cmbSelect.DisplayMember = "CompanyName";
+                cmbSelect.ValueMember = "ShipperID";
+                cmbSelect.DataSource = dt;
+
+
+                reader.Close();
+
+                
+            }
+
+            catch(Exception ex)
+            {
+                handleExeption(ex);
+            }
+
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            string CompanyName = tbAirplane.Text;
+            string Phone = tbPhone.Text;
+           
+
+            if ((CompanyName == "") || (Phone == "")  )
+            {
+                string msg = "No textbox can be empty";
+                string caption = "Error!!";
+                MessageBox.Show(msg,caption,MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+            }
+
+            else
+            {
+                conn.ConnectionString = conString;
+                cmd = conn.CreateCommand();
+                try
+                {
+                    string query = "Insert into Shippers values('" + CompanyName + "','" + Phone + "');";
+
+
+                    cmd.CommandText = query;
+                    conn.Open();
+                    cmd.ExecuteScalar();
+
+                    MessageBox.Show("Added successfully");
+
+                }
+
+                catch (Exception ex)
+                {
+                    handleExeption(ex);
+                }
+
+                finally
+                {
+                    cmd.Dispose();
+                    conn.Close();
+                }
+
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            refreshData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // delete from shippers  where ID=???
+
+            int flID = Convert.ToInt32(cmbSelect.SelectedValue);
+            conn.ConnectionString = conString;
+            cmd = conn.CreateCommand();
+            try
+            {
+                string query = "Delete from Shippers where ShipperID="+flID;
+                cmd.CommandText = query;
+                conn.Open();
+                cmd.ExecuteScalar();
+
+            }
+            catch(Exception ex)
+            {
+                handleExeption(ex);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
+    }
+}
